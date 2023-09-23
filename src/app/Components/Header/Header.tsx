15aeb011d29411c1d2e18e3695c35da8cac35cea
sutarrohit <rohitsutar082@gmail.com>
2023-09-23 22:30:19 +0530

    header added

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -1,7 +1,45 @@
+"use client";
 import React from "react";
+import { MdOutlineDarkMode } from "react-icons/md";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useTheme } from "next-themes";
 
 const Header = () => {
-  return <div>Header</div>;
+  const { resolvedTheme, setTheme } = useTheme();
+
+  return (
+    <div className="flex  justify-center items-center p-4 border border-green-800 h-20 ">
+      <div className="flex justify-between w-full md:w-[80%]">
+        <div className="border border-red-400  flex gap-1 items-center">
+          <span className="flex justify-center items-center border rounded-full w-9 h-9 bg-black text-white first-line:">
+            R
+          </span>
+          <span>Rohit Sutar</span>
+        </div>
+
+        <div className="border border-gray-900 flex">
+          <ul className="hidden md:flex">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Projects</li>
+          </ul>
+
+          <button className="hidden md:block">Contact Me</button>
+          <span
+            onClick={() =>
+              setTheme(resolvedTheme === "dark" ? "light" : "dark")
+            }
+          >
+            <MdOutlineDarkMode />
+          </span>
+
+          <span className="md:hidden">
+            <GiHamburgerMenu />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Bai_Jamjuree } from "next/font/google";
 
+import Providers from "./providers";
+
 const bai_jamjuree = Bai_Jamjuree({
   subsets: ["latin"],
   style: ["italic", "normal"],
@@ -19,8 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={bai_jamjuree.className}>{children}</body>
+    <html lang="en" className="light" style={{ colorScheme: "light" }}>
+      <body
+        className={`${bai_jamjuree.className}  bg-[#dedede] dark:bg-black dark:text-white text-black`}
+      >
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
