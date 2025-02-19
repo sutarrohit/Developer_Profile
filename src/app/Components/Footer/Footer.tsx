4acd947f9240a1d2880d14349a9ab058dd854eef
sutarrohit <rohitsutar082@gmail.com>
2025-02-19 16:42:04 +0530

    update : change copyright year to 2025

--- src/app/Components/Footer/Footer.tsx
+++ src/app/Components/Footer/Footer.tsx
@@ -1,45 +1,34 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <div className="h-[10rem] border-t-2 border-textColor flex justify-center">
-      <div className="w-full md:w-[80%] flex flex-col-reverse md:flex-row justify-evenly md:justify-around items-center">
-        <div>
-          <p className="font-bold">
-            © Copyright 2024 - Developed by Rohit Sutar. All right reserved.
-          </p>
+    return (
+        <div className='h-[10rem] border-t-2 border-textColor flex justify-center'>
+            <div className='w-full md:w-[80%] flex flex-col-reverse md:flex-row justify-evenly md:justify-around items-center'>
+                <div>
+                    <p className='font-bold'>© Copyright 2025 - Developed by Rohit Sutar. All right reserved.</p>
+                </div>
+                <div className='font-bold '>
+                    <ul className='flex  gap-8'>
+                        <li>
+                            <a href='#home' className='hover:text-black dark:hover:text-white transition-all duration-300'>
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#skills' className='hover:text-black dark:hover:text-white transition-all duration-300'>
+                                Skills
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#projects' className='hover:text-black dark:hover:text-white transition-all duration-300'>
+                                Projects
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div className="font-bold ">
-          <ul className="flex  gap-8">
-            <li>
-              <a
-                href="#home"
-                className="hover:text-black dark:hover:text-white transition-all duration-300"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#skills"
-                className="hover:text-black dark:hover:text-white transition-all duration-300"
-              >
-                Skills
-              </a>
-            </li>
-            <li>
-              <a
-                href="#projects"
-                className="hover:text-black dark:hover:text-white transition-all duration-300"
-              >
-                Projects
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Footer;
